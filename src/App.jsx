f27ca615f85c6a1851a44936011ea04cde6c5408
ManuuelG <manuelguerreron93@gmail.com>
2023-11-17 09:23:30 +0100

    ejercicio sin resolver

--- src/App.jsx
+++ src/App.jsx
@@ -9,31 +9,37 @@ function App() {
       <Stack gap={4} className="center-container">
         <div className="border-container">
           {' '}
-          <div className="p-2">SIMULA TU CRÉDITO</div>
+          <h1 className="p-2">SIMULA TU CRÉDITO</h1>
           <div className="p-2">
-            <InputGroup>
-              <InputGroup.Text>$</InputGroup.Text>
-              <Form.Control aria-label="Amount (to the nearest dollar)" />
-              <InputGroup.Text>.00</InputGroup.Text>
-            </InputGroup>
             MONTO TOTAL
             <Slider
-              size="small"
+              min={5000}
+              max={50000}
+              size="medium"
               defaultValue={70}
               aria-label="Small"
               valueLabelDisplay="auto"
               sx={{ color: 'white' }}
             />
+            <InputGroup>
+              <Form.Control aria-label="Amount (to the nearest dollar)" />
+            </InputGroup>
           </div>
           <div className="p-2">
             PLAZO
             <Slider
-              size="small"
+              min={3}
+              max={24}
+              size="medium"
               defaultValue={70}
               aria-label="Small"
               valueLabelDisplay="auto"
               sx={{ color: 'white' }}
             />
+            <InputGroup>
+              <InputGroup.Text></InputGroup.Text>
+              <Form.Control aria-label="Amount (to the nearest dollar)" />
+            </InputGroup>
           </div>
           <div className="p-2">CUOTA FIJA POR MES</div>
         </div>
